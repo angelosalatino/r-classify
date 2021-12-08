6cab083d40a8fd36c7c8fa4e7afd484fc1f7fac6
Tanay Aggarwal <aggarwalnaman54@gmail.com>
2021-12-08 12:27:50 +0530

    Update forms.py

--- textinput/forms.py
+++ textinput/forms.py
@@ -5,7 +5,4 @@ class abstractForm(forms.Form):
 
 class pdfForm(forms.Form):
     pdf_paper = forms.FileField(widget=forms.FileInput(attrs={'accept':'.pdf'}))
-    # title_pdf = forms.CharField(widget=forms.Textarea(attrs={"label":"Abstract<span class='req'>*</span>", "rows":2, "cols":50, "id":'title_input'}))
-    # abstract_pdf = forms.CharField(widget=forms.Textarea(attrs={"label":"Abstract<span class='req'>*</span>", "rows":10, "cols":50, "id":'abstract_input'}))
-    # keywords_pdf = forms.CharField(widget=forms.Textarea(attrs={"label":"Abstract<span class='req'>*</span>", "rows":2, "cols":50, "id":'keywords_input'}))
 
